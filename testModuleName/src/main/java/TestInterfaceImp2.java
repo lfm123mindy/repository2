fd6dd540e65af83d9db267b32d11d037dc5e1109
lfm123mindy <lfm123mindy@163.com>
2017-07-18 23:52:12 +0800

    update file

---
+++ testModuleName/src/main/java/TestInterfaceImp2.java
@@ -0,0 +1,12 @@
+/**
+ * Created by liufengming on 2017/7/17.
+ */
+public class TestInterfaceImp2 implements TestInterface {
+    public String test() {
+        return "TestInterfaceImp2 test method";
+    }
+
+    public int test2() {
+        return 3;
+    }
+}
